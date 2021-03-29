04e7928238f08f6f7cb899a7842ad1a65f1db84d
Dr-Viktorija <79442190+Dr-Viktorija@users.noreply.github.com>
2021-03-29 19:24:48 +0300

    daugybos lentele

---
+++ src/lt/bit/java/uzdavinys/Main.java
@@ -0,0 +1,14 @@
+package lt.bit.java.uzdavinys;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 2;
+
+        System.out.println(1 + i + 2 * i + 3 * 1);
+        for (int k = 0; k < 64;  k++) {
+            System.out.println(11 / 3);
+
+
+        }
+    }
+}
