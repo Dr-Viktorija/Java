67a083d499fdf618842d78cfda82be89b973e231
Dr-Viktorija <79442190+Dr-Viktorija@users.noreply.github.com>
2021-03-30 14:50:43 +0300

    daugybos lentele pataisyta

--- src/lt/bit/java/uzdavinys2/Main.java
+++ src/lt/bit/java/uzdavinys2/Main.java
@@ -1,15 +1,54 @@
 package lt.bit.java.uzdavinys2;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        int i;
+//        int i;
+//
+//        for (i = 1; i <= 10; i++) {
+////            System.out.println(i );
+//            int n;
+//            for (n = 1; n <=10; n++) {
+//                System.out.println(i + " x " + n + " = " + (i * n));
+//            }
+//            System.out.println();
+//        }
+//     for(int row = 1; row <= 10; row++) {
+//         for (int col = 1; col <= 9; col++) {
+//             System.out.print(col + " x " + row + " = " + col * row + "\t");
+//         }
+//         System.out.println();
+//     }
+//        for(int row = 1; row <= 10; row++) {
+//            for (int col = 1; col <= 3; col++) {
+//                System.out.print(col + " x " + row + " = " + col * row + "\t");
+//            }
+//            System.out.println();}
+//        System.out.println();
+//            for(int row = 1; row <= 10; row++) {
+//                for (int col = 4; col <= 6; col++) {
+//                    System.out.print(col + " x " + row + " = " + col * row + "\t");
+//                }
+//                System.out.println();
+//            }
+//        System.out.println();
+//            for(int row = 1; row <= 10; row++) {
+//        for (int col = 7; col <= 9; col++) {
+//            System.out.print(col + " x " + row + " = " + col * row + "\t");
+//        }
+//        System.out.println();
+//    }
 
-        for (i = 1; i <= 10; i++) {
-//            System.out.println(i );
-            int n;
-            for (n = 1; n <=10; n++) {
-                System.out.println(i + " x " + n + " = " + (i * n));
+        for(int i = 0; i < 3; i++) {
+            for (int row = 1; row <= 10; row++) {
+                for(int col = i * 3 + 1; col <= i * 3 + 1 + 2; col++) {
+                    System.out.print(col + " x " + row + " = " + col * row + "\t");
+                }
+                System.out.println();
             }
+            System.out.println();
+        }
         }
     }
-}
+
