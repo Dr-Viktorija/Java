2d34dc88d9063a3bcadf4b6331c700bd3cbe081d
Dr-Viktorija <79442190+Dr-Viktorija@users.noreply.github.com>
2021-03-31 15:51:48 +0300

    masyvo surikiavimas

---
+++ src/lt/bit/java/uzdavinys4/Main.java
@@ -0,0 +1,24 @@
+package lt.bit.java.uzdavinys4;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int masyvas[] = {5, 4, -2, 0, 1, 88, -2, 10, 4, 9};
+        int k = 0;
+
+        System.out.println(Arrays.toString(masyvas));
+
+        for(int i = 0; i < masyvas.length; i++) {
+            for(int j = i; j < masyvas.length; j++) {
+                if(masyvas[i] > masyvas[j]) {
+                    k = masyvas[i];
+                    masyvas[i] = masyvas[j];
+                    masyvas[j] = k;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(masyvas));
+    }
+}
